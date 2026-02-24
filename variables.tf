b1cff2d50164996283767d1eecb1dcff8bd7578e
agallego <agallego@pragma.com.co>
2026-02-23 20:58:02 -0500

    revert prevent destroy

--- variables.tf
+++ variables.tf
@@ -70,7 +70,6 @@ variable "dynamo_config" {
     range_key                   = optional(string)
     point_in_time_recovery      = optional(bool, true)
     deletion_protection_enabled = optional(bool, true)
-    prevent_destroy             = optional(bool, true)
 
     attributes = list(object({
       name = string
